8c39da24c6f0f867e5ee486a9833d4820b922a6b
elektronicznypank <dizelpauer115@gmail.com>
2023-02-09 21:22:57 +0100

    Different approach.

--- exercises/if_string_is_palindrome.py
+++ exercises/if_string_is_palindrome.py
@@ -1,14 +1,19 @@
-# Write a function that checks if string is palindrome.
+# Write a function that checks if a string is a palindrome.
 
-def if_string_is_palindrome(string: str = input('Podaj ciąg znaków, który chcesz sprawdzić: ')):
+from string import punctuation as pct
+
+
+def if_string_is_palindrome(string: str = input('Podaj ciąg liter i/lub cyfr, który chcesz sprawdzić: ')):
+    for i in string[::-1]:
+        if i in pct:
+            string = string.replace(i, '')
     rev_string: str = string[::-1]
     if string == '':
         return 'String jest pusty.'
     else:
-        return 'Podany ciąg znaków jest palindromem.'\
-            if rev_string.lower().replace(' ', '').replace('.', '').replace(',', '')\
-            == string.lower().replace(' ', '').replace('.', '').replace(',', '')\
-            else 'Podany ciąg znaków nie jest palindromem.'
+        return 'Podany ciąg liter jest palindromem.'\
+            if rev_string.lower() == string.lower()\
+            else 'Podany ciąg liter nie jest palindromem.'
 
 
 print(if_string_is_palindrome())
